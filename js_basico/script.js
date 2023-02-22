f14c5038c06a83fc76738a4d4556e0663e1d1f6c
GuilhermeAlquimim <guigui072004@gmail.com>
2023-02-22 16:37:32 -0300

    conclusão até o mini-projeto JS 4

--- js_basico/script.js
+++ js_basico/script.js
@@ -1,6 +1,26 @@
-// Conceitos básicos de JS
+// Desafio JS: Criar um código que retorna:
+// nome de um aluno dentre um grupo, suas notas, conceito e aprovação.
 
-var a = "10";
-var b = "20";
+let alunos = [`Guizão`, `Brunão`, `Bielzao`, `Pedrão`, `Giovanna`];
 
-console.log(parseInt(a) + parseInt(b));
+function pegarDuasNotas() {
+    return [Math.random()*10, Math.random()*10]
+}
+
+for (let i of alunos) {
+    let [nota1, nota2] = pegarDuasNotas();
+
+    let media = (parseInt(nota1) + parseInt(nota2)) /2;
+
+    let conceito
+
+    if (media >= 6) {
+        conceito = `Aprovado!`
+    }
+
+    else {
+        conceito = `Reprovado!`
+    }
+
+    console.log(`${i} com as notas ${parseInt(nota1)} e ${parseInt(nota2)}, obteve o conceito ${media}, portanto está ${conceito}`)
+}
