1ef36485bb36127c8c1663b14c176cac68c3741c
GuilhermeAlquimim <guigui072004@gmail.com>
2023-02-27 16:19:55 -0300

    Progresso JS Intermediário

--- js.intermediario/script.js
+++ js.intermediario/script.js
@@ -7,3 +7,16 @@ function hideh1() {
   let h1 = document.getElementsByTagName("h1")[0];
   h1.style.visibility = "hidden";
 }
+
+function appearh1() {
+  let h1 = document.getElementsByTagName("h1")[0];
+  h1.style.visibility = "visible";
+}
+
+function onOver(element) {
+  element.style.backgroundColor = "red";
+}
+
+function onOut(element) {
+  element.style.backgroundColor = "purple";
+}
